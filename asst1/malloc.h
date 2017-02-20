0a963aee5ffee965c090c91d44247a77b49884e6
Mohit Mori <mohitmori1997@gmail.com>
2017-02-20 05:23:45 -0500

    initial header commit

---
+++ asst1/malloc.h
@@ -0,0 +1,11 @@
+#ifndef MALLOC_H_
+#define MALLOC_H_
+
+#define MEM_SIZE 5000
+#define malloc(x) mymalloc(x, __FILE__,__LINE__)
+#define free(x) myfree(x,__FILE__,__LINE__)
+
+void* mymalloc(int,char*,int);
+void myfree(void*,char*,int);
+
+#endif
