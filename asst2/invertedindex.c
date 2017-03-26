b0ad6195c482db3adc8afa11571fb39c2ec1a4fc
mattplatoff <mattplatoff@gmail.com>
2017-03-25 21:23:53 -0400

    added recursive indexer, needs absoulte path soultion

--- asst2/invertedindex.c
+++ asst2/invertedindex.c
@@ -8,11 +8,31 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+#define PATH_MAX 4096 
+
+
+typedef struct fileTokenCount{
+	char* fileName;
+	int count;
+} fileTokenCount;
+
+typedef struct indexMap{
+	char* token;
+	fileTokenCount files[10];
+} indexMap;
+
 int isFile(const char* path)
 {
     struct stat path_stat;
     stat(path, &path_stat);
     return S_ISREG(path_stat.st_mode);
+}
+
+int isDir(const char* path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISDIR(path_stat.st_mode);
 }
 
 void tokenize(int fd)
@@ -43,7 +63,82 @@ void tokenize(int fd)
 		token = NULL;
 		i = 0;
 	}
+
 }
+
+ void recursiveIndex(const char* dirName, DIR* dstream, char * path){
+	//Need to find way to maintain absoulte path as we go into child directories , 
+	//currently program has issues because it searches for target in current working directory insted of the child directory
+
+	printf("recursive index called\n");
+	DIR* directory;
+	
+	
+	if (dstream==NULL){
+		printf("dir stream was null\n");
+	directory = opendir(dirName);
+	if (directory==NULL){
+		printf("directory was empty\n");
+		return;
+	}}
+	else{
+		directory = dstream;
+	}
+
+	char fullpath[PATH_MAX];
+	
+	if (strcmp("",path)==0){
+		getcwd(fullpath,PATH_MAX);
+	}
+	path=fullpath;
+	
+	
+// get next element in dir
+	struct dirent* entry= readdir(directory);
+	// while ((strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)){
+	// 	entry = readdir(directory);
+	// }
+	//need to make it so that when recursive index is called on a subdirectory, the path is updated so that it is still relitive to the working directory
+	
+//ignore refrences to hidden files and placeholders
+	while(entry->d_name[0]=='.'){
+		entry = readdir(directory);
+	}
+			// char * newPath;
+			// newPath = malloc(strlen(entry->d_name)+strlen(fullpath)+2);
+   //  		newPath[0] = '\0';   // ensures the memory is an empty string
+   //  		strcat(newPath,fullpath);
+   //  		strcat(newPath,entry->d_name);
+    		
+    		//no elements left in dir
+		if (entry == NULL){
+			printf("entry was null \n");
+			closedir(directory);
+			return;
+		}
+		//if dirent is file, pass file to tokenizer, pass dirstream back to recursive index to process next element in this directory. 
+		else if (isFile(newPath)){
+			printf("%swas a file\n",entry->d_name);
+			int fd = open(entry->d_name,O_RDONLY);
+			tokenize(fd);
+			recursiveIndex(NULL,directory,fullpath);
+			return;
+		}
+		//if file from stream is a child directory, call recursiveIndex on that directory and pass in dirstream to that directory. 
+		else if (isDir(newPath)){
+			printf("%swas a dir\n", entry->d_name);
+			DIR* childDir = opendir(entry->d_name);
+			//strcat(newPath,"/");
+    		printf("%snew path is \n", newPath);
+			recursiveIndex(NULL,childDir,newPath);
+			return;
+		}
+		else{
+			printf("there was a problem with file %s\n", entry->d_name);
+		}
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -63,7 +158,12 @@ int main(int argc, char** argv)
 	}
 
 	//if 2nd argument is a directory(will handle recursive directory traversal after program is working with a single file)
+	else if (isDir(argv[2])){
+		recursiveIndex(argv[2],NULL,"");
+	}
+	
 	else{
+		printf("thats a bop\n");
 		struct dirent * currObj = NULL;
 		DIR* directory = opendir(argv[2]);
 		currObj = readdir(directory);
